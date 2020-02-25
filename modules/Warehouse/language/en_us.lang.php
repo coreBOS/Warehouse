bca9c6e5fe06f8b066e7a56f9a02bb546d56a3cb
joebordes <joe@tsolucio.com>
2020-02-25 13:26:34 +0100

    initial release

---
+++ modules/Warehouse/language/en_us.lang.php
@@ -0,0 +1,34 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'Warehouse' => 'Warehouse',
+	'SINGLE_Warehouse' => 'Warehouse',
+	'Warehouse ID' => 'Warehouse ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_WAREHOUSE_INFORMATION' => 'Warehouse Information',
+	'LBL_ADDRESS_INF' => 'Address Information',
+	'LBL_DESCRIPTION_INFORMATION' => 'Description',
+
+	'WarehouseNo' => 'Warehouse No',
+	'Title' => 'Title',
+	'LBL_PHONE' => 'Phone',
+	'Fax' => 'Fax',
+	'E-mail' => 'E-mail',
+	'Address' => 'Address',
+	'City' => 'City',
+	'State' => 'State',
+	'Zip Code' => 'Zip Code',
+	'CannotDelete' => 'This warehouse cannot be deleted, it is used by the application.',
+	'Warehouse ID' => 'Warehouse ID',
+);
+
+?>
---
+++ modules/Warehouse/language/es_es.lang.php
@@ -0,0 +1,33 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'Warehouse' => 'Almacén',
+	'SINGLE_Warehouse' => 'Almacén',
+	'Warehouse ID' => 'Almacén ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Información Personalizada',
+	'LBL_WAREHOUSE_INFORMATION' => 'Información del Almacén',
+	'LBL_ADDRESS_INF' => 'Dirección',
+	'LBL_DESCRIPTION_INFORMATION' => 'Descripción',
+
+	'WarehouseNo' => 'No Almacén',
+	'Title' => 'Nombre',
+	'LBL_PHONE' => 'Teléfono',
+	'Fax' => 'Fax',
+	'E-mail' => 'E-mail',
+	'Address' => 'Dirección',
+	'City' => 'Ciudad',
+	'State' => 'Provincia',
+	'Zip Code' => 'Código Postal',
+	'CannotDelete'=>'Este almacén no se puede eliminar, se utiliza para gestión interna.',
+);
+
+?>
